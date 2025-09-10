87b3485ddde62d81633de8e9cf8a317d866e0e00
mvusic07 <thinkman405@gmail.com>
2025-09-10 19:18:42 +0200

    parsing

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,10 +1,17 @@
 package request
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"io"
-	"strings"
+)
+
+type praserState string
+
+const (
+	StateInit praserState = "init"
+	StateDone praserState = "done"
 )
 
 type RequestLine struct {
@@ -15,45 +22,89 @@ type RequestLine struct {
 
 type Request struct {
 	RequestLine RequestLine
+	state       praserState
 }
 
-var SEPERATOR = "\r\n"
+func newRequest() *Request {
+	return &Request{
+		state: StateInit,
+	}
+}
 
-func parseRequestLine(b string) (*RequestLine, string, error) {
-	ind := strings.Index(b, SEPERATOR)
+func (r *Request) parse(data []byte) (int, error) {
+	read := 0
+outer:
+	for {
+		switch r.state {
+		case StateInit:
+			rl, n, err := parseRequestLine(data[read:])
+			if err != nil {
+				return 0, err
+			}
+			if n == 0 {
+				break outer
+			}
+			r.RequestLine = *rl
+			read += n
+			r.state = StateDone
+		case StateDone:
+			break outer
+		}
+	}
+	return read, nil
+}
+
+func (r *Request) done() bool {
+	return r.state == StateDone
+}
+
+var SEPERATOR = []byte("\r\n")
+var SEP = []byte("/")
+
+func parseRequestLine(b []byte) (*RequestLine, int, error) {
+	ind := bytes.Index(b, SEPERATOR)
 	if ind == -1 {
-		return nil, b, nil
+		return nil, 0, nil
 	}
 
-	startLine := b[:ind]                // npr GET / HTTP / 1.1 \r\n -- kraj ..........
-	restOfMsq := b[ind+len(SEPERATOR):] // field lines
+	startLine := b[:ind] // npr GET / HTTP / 1.1 \r\n -- kraj ..........
+	read := ind + len(SEPERATOR)
 
-	parts := strings.Fields(startLine)
+	parts := bytes.Fields(startLine)
 	if len(parts) != 3 { // if we don't have method,version, http proctocol
-		return nil, b, errors.Join(fmt.Errorf("don't have all the parts of the request line"))
+		return nil, 0, errors.Join(fmt.Errorf("don't have all the parts of the request line"))
 	}
 
-	httpPart := strings.Split(parts[2], "/")
-	if len(httpPart) != 2 || httpPart[0] != "HTTP" || httpPart[1] != "1.1" {
-		return nil, b, errors.Join(fmt.Errorf("http version not valid"))
+	httpPart := bytes.Split(parts[2], SEP)
+	if len(httpPart) != 2 || string(httpPart[0]) != "HTTP" || string(httpPart[1]) != "1.1" {
+		return nil, 0, errors.Join(fmt.Errorf("http version not valid"))
 	}
 
 	rl := &RequestLine{
-		Method:        parts[0],
-		RequestTarget: parts[1],
-		HttpVersion:   httpPart[1],
+		Method:        string(parts[0]),
+		RequestTarget: string(parts[1]),
+		HttpVersion:   string(httpPart[1]),
 	}
-	return rl, restOfMsq, nil
+	return rl, read, nil
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	data, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, errors.Join(fmt.Errorf("unable to io.ReadAll"), err)
+	request := newRequest()
+	buf := make([]byte, 1024)
+	bufLen := 0
+	for !request.done() {
+		n, err := reader.Read(buf[bufLen:])
+		if err != nil {
+			return nil, errors.Join(fmt.Errorf("error while reading"), err)
+		}
+		bufLen += n
+		readN, err := request.parse(buf[:bufLen])
+		if err != nil {
+			return nil, err
+		}
+		copy(buf, buf[readN:bufLen])
+		bufLen -= readN
+
 	}
-	rl, _, err := parseRequestLine(string(data))
-	if err != nil {
-		return nil, errors.Join(fmt.Errorf("unable to parse request line"), err)
-	}
-	return &Request{RequestLine: *rl}, nil
+	return request, nil
 }
